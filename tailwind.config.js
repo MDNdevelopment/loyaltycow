39dc7b256aca1ac5bde5351d359da4d9afe39a66
Juan <juanluislauretta@gmail.com>
2026-03-23 17:19:03 -0400

    feat: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navbar: {
+          DEFAULT: "#202020",
+        },
+        primary: {
+          800: "#BE1E2D",
+          600: "#a61b29",
+        },
+        secondary: {
+          DEFAULT: "#BE1E2D",
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
